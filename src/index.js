fed87a77b336d9bc813100472623505807b20c82
EzzeGuason <ezze.juarezz@gmail.com>
2020-10-14 12:10:24 -0300

    Se añade una conexión a un bd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const {createWindow} = require('./app')
 const {app} = require('electron')
 
+require('./database')
+require('electron-reload')(__dirname)
+
+
 app.whenReady()
 .then(createWindow)
 
